c6956b6034df824457c4cc2f57fe3a5285d781d9
TingSyuanWang <dtvgood202@gmail.com>
2017-06-04 05:16:22 +0800

    timezone proble solved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,8 +44,8 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">管理員登入</a></li>
-                        @else
                             <li><a href="{{ route('register') }}">新增帳號</a></li>
+                        @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
